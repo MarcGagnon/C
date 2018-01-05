2313b57ad5baa394e3d5cb0e9809e06a7b35933e
U-CLG\Marc.Gagnon <Marc.Gagnon@M-BUREAUTI-MG2.clg.priv>
2018-01-05 09:22:12 -0500

    Cygwin: Création de emptyvars.c et emptyvars.exe

---
+++ emptyvars.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+    int intVide;
+    char charVide;
+
+    printf("La valeur de intVide est : %i\n", intVide);
+    printf("La valeur de charVide est : %s\n", charVide);
+} 
