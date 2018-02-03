ca97a0c81713dd4afc2450e2383b421f04035983
Marc Gagnon <marco.gagnon@gmail.com>
2018-02-03 13:34:52 -0500

    Ajout 1-31.c et bonjourVSC.c

---
+++ divers/1-31.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	system("cls");
+	printf("Ceci est un programme écrit en C (1)\n");
+	getchar();
+	printf("Ceci est un programme écrit en C (2)\n");
+	getchar();
+	printf("Ceci est un programme écrit en C (3)\n");
+	getchar();
+	return(0);
+}
